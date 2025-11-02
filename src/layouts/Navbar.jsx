6425b5216e9a71f4c28a928940e4e8f29c0f8ed2
Mahmud Hasan <mh.kid234@gmail.com>
2025-11-02 18:59:07 +0600

    made Navbar profile pic placeholder logged in to meaningful profile placeholder

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -85,7 +85,7 @@ const Navbar = () => {
                   <div className="w-10 rounded-full">
                     <img
                       alt="User Avatar"
-                      src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                      src="https://www.gravatar.com/avatar/?d=mp&f=y"
                     />
                   </div>
                 </div>
