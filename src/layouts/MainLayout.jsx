5b9bf73d90a2a17387b41095b6be9129ad51f88f
Mahmud Hasan <mh.kid234@gmail.com>
2026-04-27 16:42:41 +0600

    fixing empty space in somephone at the right side

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,11 +4,15 @@ import { Outlet } from "react-router";
 
 const MainLayout = () => {
   return (
-    <>
-    <Navbar />
-    <Outlet />
-    <Footer />
-    </>
+    // "overflow-x-hidden" is the magic pill here. 
+    // "flex flex-col min-h-screen" ensures the footer stays at the bottom.
+    <div className="flex flex-col min-h-screen overflow-x-hidden relative">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
   )
 }
 
